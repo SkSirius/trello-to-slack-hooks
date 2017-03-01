c676647cb12896abe0d2b12252ed9f82fb9ac410
Pavlo Sobchuk <pavlo.sobchuk@gmail.com>
2017-03-01 18:19:01 +0200

    changes

--- trello-service.js
+++ trello-service.js
@@ -35,7 +35,7 @@ var processChange = function(data) {
     var model = data.model;
     var action = data.action;
 
-    console.log(data.action.card);
+    console.log(action.data.card);
 
     var slackModel = {
         boardName: model.name,
